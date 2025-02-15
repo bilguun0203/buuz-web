11ea05b137f087cdbabf1cd2807ef13e67d54f19
Bilguun Ochirbat <bilguun0203@gmail.com>
2025-02-15 23:03:37 +0800

    build: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import preact from "@astrojs/preact";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), icon()]
 });
